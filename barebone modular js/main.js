6d43369808b72a06fb9aaec184f8c8b6432d92c2
Sudeep kuchara <sudeepzmr@gmail.com>
2021-12-29 19:56:08 +0530

    upload barebone js modules folder

---
+++ barebone modular js/main.js
@@ -0,0 +1,3 @@
+import domchange from '/modules/module1.mjs'
+
+domchange();
---
+++ barebone modular js/modules/module1.mjs
@@ -0,0 +1,4 @@
+export default function domchange() {
+    console.log("hey")
+    return document.getElementById('elm').innerHTML = "yoloing js modules on browser!"
+}
